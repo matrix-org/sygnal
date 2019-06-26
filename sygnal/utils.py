1c7101342a61e61accce285aaf4d7a8f26ec3e85
reivilibre <olivier@librepush.net>
2019-06-26 14:33:03 +0100

    Add docstring for twisted_sleep

--- sygnal/utils.py
+++ sygnal/utils.py
@@ -3,6 +3,17 @@ from twisted.internet.defer import Deferred
 
 
 async def twisted_sleep(delay, twisted_reactor=twisted.internet.reactor):
+    """
+    Creates a Deferred which will fire in a set time.
+    This allows you to `await` on it and have an async analogue to
+    L{time.sleep}.
+    Args:
+        delay: Delay in seconds
+        twisted_reactor: Reactor to use for sleeping.
+
+    Returns:
+        a Deferred which fires in `delay` seconds.
+    """
     deferred = Deferred()
     twisted_reactor.callLater(delay, deferred.callback, None)
     await deferred
