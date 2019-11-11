30aeee4f0290ff394e97bbf004b96755a385f19b
Johannes Erlacher <johannes.erlacher@bytepoets.com>
2019-11-11 09:51:29 +0100

    [APP-730] refactor firebase pushkin; adds logging

--- sygnal/firebasepushkin.py
+++ sygnal/firebasepushkin.py
@@ -18,12 +18,14 @@ import logging
 from typing import Dict, Optional
 
 import attr
+import asyncio
+from opentracing import logs, tags
 from firebase_admin import credentials, initialize_app, messaging
-from firebase_admin.exceptions import FirebaseError
+from firebase_admin.exceptions import *
 from prometheus_client import Histogram
 from twisted.internet.defer import Deferred
 from twisted.python.threadpool import ThreadPool
-from sygnal.utils import twisted_sleep
+from sygnal.utils import twisted_sleep, NotificationLoggerAdapter
 
 from .exceptions import PushkinSetupException, TemporaryNotificationDispatchException, NotificationDispatchException
 from .notifications import Pushkin
@@ -37,11 +39,6 @@ NOTIFICATION_DATA_INCLUDED = [
     'room_id',
     'event_id',
     'sender_display_name',
-#    'sender',
-#    'room_name',
-#    'room_alias',
-#    'membership',
-#    'content',
 ]
 
 logger = logging.getLogger(__name__)
@@ -112,12 +109,12 @@ class FirebasePushkin(Pushkin):
     def _map_counts_unread(n):
         return n.counts.unread or 0
 
-    def _map_request_message(self, n, device):
+    async def _dispatch_message(self, n, device, span, log):
+        log.info("dispatching message")
         notification_title = n.room_name or n.sender_display_name
         notification_body = self._map_notification_body(n).strip()
         notification = messaging.Notification(title=notification_title, body=notification_body)
 
-        # TODO: remove duplicated code
         android = messaging.AndroidConfig(priority=self._map_android_priority(n),
                                           notification=messaging.AndroidNotification(
                                               click_action="FLUTTER_NOTIFICATION_CLICK",
@@ -136,11 +133,10 @@ class FirebasePushkin(Pushkin):
             apns=apns,
             token=device.pushkey,
         )
-        return request
+        return await self._dispatch(request, span, log)
 
-    def _map_request_event(self, n, device):
-
-        # TODO: remove duplicated code
+    async def _dispatch_event(self, n, device, span, log):
+        logger.info("dispatching event")
         android = messaging.AndroidConfig(priority=self._map_android_priority(n))
 
         apns = messaging.APNSConfig(
@@ -156,34 +152,59 @@ class FirebasePushkin(Pushkin):
             apns=apns,
             token=device.pushkey
         )
-        return request
+        return await self._dispatch(request, span, log)
 
-    async def _dispatch_message(self, n, device):
-        logger.info("dispatching message")
-        return self._send(self._map_request_message(n, device), device)
+    async def _dispatch(self, request, span, log):
+        if request.data is None and request.notification:
+            span.log_kv({logs.EVENT: "firebase_no_payload"})
 
-    async def _dispatch_event(self, n, device):
-        logger.info("dispatching event")
-        return self._send(self._map_request_event(n, device), device)
+        log.info(f"Sending Firebase message")
+        try:
+            with SEND_TIME_HISTOGRAM.time():
+                response = await self._send_notification(request)
+        except FirebaseError as e:
+            span.set_tag("firebase_reason", e.cause)
+            log.info(f"{e}")
+            if e.code is UNAVAILABLE:
+                error = f"FirebaseError: {e.code} {e.cause}"
+                raise TemporaryNotificationDispatchException(error)
+            else:
+                error = f"FirebaseError: {e.code} {e.cause}"
+                raise NotificationDispatchException(error)
+        except ValueError as e:
+            log.info(f"{e}")
+            span.set_tag("firebase_reason", e)
+            error = f"ValueError: {e}"
+            raise NotificationDispatchException(error)
 
-    def _map_event_dispatch_handler(self, n):
+        log.info("Success sending Firebase message")
+        span.set_tag("firebase_id", response)
+        return []
+
+    def _map_event_dispatch_handler(self, n, log):
         event_handlers = self.config.event_handlers
         if not event_handlers:
             if n.type != "m.room.message" or n.content["msgtype"] not in self.config.message_types:
+                log.info("event handlers not specified, no valid type or message type found")
                 return None
             else:
+                log.info("event handlers not specified, using message handler")
                 return self._dispatch_message
         else:
             handler = event_handlers.get(n.type, None)
             if handler == "message":
+                log.info("using message handler")
                 return self._dispatch_message
             elif handler == "event":
+                log.info("using event handler")
                 return self._dispatch_event
             else:
                 return None
 
     async def dispatch_notification(self, n, device, context):
-        dispatch_handler = self._map_event_dispatch_handler(n)
+        log = NotificationLoggerAdapter(logger, {"request_id": context.request_id})
+
+        dispatch_handler = self._map_event_dispatch_handler(n, log)
         if dispatch_handler is None:
             return []  # skipped
 
@@ -191,9 +212,13 @@ class FirebasePushkin(Pushkin):
         with self.sygnal.tracer.start_span(
                 "firebase_dispatch", tags=span_tags, child_of=context.opentracing_span
         ) as span_parent:
+
             for retry_number in range(self.MAX_TRIES):
                 try:
-                    return await dispatch_handler(n, device)
+                    with self.sygnal.tracer.start_span(
+                            "firebase_dispatch_try", tags=span_tags, child_of=span_parent
+                    ) as span:
+                        return await dispatch_handler(n, device, span, log)
                 except TemporaryNotificationDispatchException as ex:
                     retry_delay = self.RETRY_DELAY_BASE * (2 ** retry_number)
                     if ex.custom_retry_delay is not None:
@@ -215,18 +240,8 @@ class FirebasePushkin(Pushkin):
                             retry_delay, twisted_reactor=self.sygnal.reactor
                         )
 
-    def _send(self, request, device):
-        try:
-            # TODO: response returns message id if successful
-            response = messaging.send(request, app=self._app)
-        except FirebaseError as e:
-            logger.error(f"error while sending notification: {e}")
-            # TODO: Differentiate different errors and if retry should apply
-        except ValueError as e:
-            logger.error(f"value error in sending notification {e}")
-
-        logger.info("Message sent successfully")
-        return []
+    async def _send_notification(self, request):
+        return messaging.send(request, app=self._app)
 
     @staticmethod
     def _map_android_priority(n):
@@ -239,11 +254,9 @@ class FirebasePushkin(Pushkin):
 
 def build_data_for_notification(n):
     data = {}
-    logger.info("building data")
     for field in NOTIFICATION_DATA_INCLUDED:
         if hasattr(n, field) and getattr(n, field) is not None:
             data[field] = getattr(n, field)
-    logger.info("building data finished")
     return data
 
 
