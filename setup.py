0ced8bda3c759b1584d327205245162da64488a4
Richard van der Hoff <richard@matrix.org>
2020-03-24 16:17:30 +0000

    Use setuptools_scm for version number
    Hardcoded version numbers are rubbish.

--- setup.py
+++ setup.py
@@ -31,9 +31,10 @@ def read(fname):
 
 setup(
     name="matrix-sygnal",
-    version=read("VERSION").strip(),
     packages=find_packages(exclude=["tests", "tests.*"]),
     description="Reference Push Gateway for Matrix Notifications",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     install_requires=[
         "Twisted>=19.2.1",
         "prometheus_client>=0.7.0,<0.8",
@@ -45,6 +46,7 @@ setup(
         "sentry-sdk>=0.10.2",
         "zope.interface>=4.6.0",
         "idna>=2.8",
+        "importlib_metadata",
     ],
     long_description=read("README.rst"),
 )
