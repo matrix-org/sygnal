40107f10770c8ade4cb9c7e236bb6f6b3b400e6c
Olivier Wilkinson (reivilibre) <olivier@librepush.net>
2020-06-26 10:21:55 +0100

    Antilint

--- sygnal/helper/asyncio_connectprotocol.py
+++ sygnal/helper/asyncio_connectprotocol.py
@@ -1,7 +1,6 @@
 import asyncio
-from asyncio import transports, Transport
-from base64 import b64encode, urlsafe_b64encode
-from functools import partial
+from asyncio import Transport
+from base64 import urlsafe_b64encode
 from typing import Optional, Tuple
 
 
@@ -14,20 +13,26 @@ class HttpConnectProtocol(asyncio.Protocol):
 
     For Twisted, see connectproxyclient.py instead.
     """
-    def __init__(self, proxy_address: str, target_address: str, protocol_on_top: asyncio.Protocol,
-                 basic_proxy_auth: Optional[Tuple[str, str]]):
+
+    def __init__(
+        self,
+        proxy_address: str,
+        target_address: str,
+        protocol_on_top: asyncio.Protocol,
+        basic_proxy_auth: Optional[Tuple[str, str]],
+    ):
         self.basic_proxy_auth = basic_proxy_auth
         self.completed = False
         self.proxy_address = proxy_address
         self.target_address = target_address
         self.protocol_on_top = protocol_on_top
-        self.buffer = b''
+        self.buffer = b""
         self.transport = None
 
     def data_received(self, data: bytes) -> None:
         super().data_received(data)
         self.buffer += data
-        if self.buffer.endswith(b'\r\n\r\n'):
+        if self.buffer.endswith(b"\r\n\r\n"):
             # warning: note this won't work if the remote host talks through
             # the tunnel first.
             # (This is OK because:
@@ -42,15 +47,15 @@ class HttpConnectProtocol(asyncio.Protocol):
             # the first line of the response headers is the Status Line
             # the response headers are terminated by a double CRLF.
             # All HTTP lines are terminated by CRLF.
-            lines = self.buffer.split(b'\r\n')
+            lines = self.buffer.split(b"\r\n")
             status_line = lines[0]
             # maxsplit=2 denotes the number of separators, not the № items
             # StatusLine ← HTTPVersion SP StatusCode SP ReasonPhrase
             # None of the fields may contain CRLF, and only ReasonPhrase may
             # contain SP.
-            [http_version, status, reason_phrase] = status_line.split(b' ', maxsplit=2)
+            [http_version, status, reason_phrase] = status_line.split(b" ", maxsplit=2)
             print(f"from proxy: hv={http_version}, s={status}, rp={reason_phrase}")
-            if status != b'200':
+            if status != b"200":
                 # 200 Successful (aka Connection Established) is what we want
                 # if it is not what we have, then we don't have a tunnel
                 print(f"ERROR from HTTP Proxy: {status} ({reason_phrase})")
@@ -74,12 +79,12 @@ class HttpConnectProtocol(asyncio.Protocol):
         if self.basic_proxy_auth is not None:
             # a credential pair is a urlsafe-base64-encoded
             (user, password) = self.basic_proxy_auth
-            encoded_credentials = urlsafe_b64encode(f'{user}:{password}')
-            transport.write(f"Proxy-Authorization: basic {encoded_credentials}\r\n".encode())
+            encoded_credentials = urlsafe_b64encode(f"{user}:{password}")
+            transport.write(
+                f"Proxy-Authorization: basic {encoded_credentials}\r\n".encode()
+            )
         # a blank line terminates the request headers
         transport.write(b"\r\n")
 
         # now we wait ...
         self.transport = transport
-
-
