db4f1d63be275305cd9d0ae40e686826e4dd26fa
Olivier Wilkinson (reivilibre) <olivier@librepush.net>
2020-07-14 10:50:18 +0100

    Fix decompose_http_proxy_url
    this morning: More Sygnal#130 (HTTP proxy) rework, I'm feeling it
    straightening out a lot so hopefully it'll be back in the queue soon;
    
    today: 'More of that'; Catch up on #2528 and #7828 which Riiich has
    suggested solving first
    Github
    https://github.com/matrix-org/synapse/issues/2528 : Homeservers don't
    catch up with missed traffic until someone sends another event
    https://github.com/matrix-org/synapse/issues/7828 : in-memory federation
    transaction transmission queues build up indefinitely for offline
    servers

--- sygnal/helper/proxy/__init__.py
+++ sygnal/helper/proxy/__init__.py
@@ -1,7 +1,7 @@
 from urllib.parse import urlparse
 
 
-def decompose_http_proxy_url(self, proxy_url):
+def decompose_http_proxy_url(proxy_url):
     url = urlparse(proxy_url, scheme="http")
 
     if not url.hostname:
@@ -13,4 +13,6 @@ def decompose_http_proxy_url(self, proxy_url):
         raise RuntimeError(
             f"Unknown proxy scheme {url.scheme};" f" only 'http' is supported."
         )
+
+    return url
 
