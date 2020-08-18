f8c37b92131dd2db4209d9efd495f7cbf2d319e4
Olivier Wilkinson (reivilibre) <olivier@librepush.net>
2020-08-18 09:47:20 +0100

    Copyright headers

--- tests/test_httpproxy_twisted.py
+++ tests/test_httpproxy_twisted.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 The Matrix.org Foundation C.I.C.
+# Copyright 2019-2020 The Matrix.org Foundation C.I.C.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -34,9 +34,9 @@ logger = logging.getLogger(__name__)
 HTTPFactory = Factory.forProtocol(HTTPChannel)
 
 
-class MatrixFederationAgentTests(TestCase):
+class SygnalTwistedProxyTests(TestCase):
     def config_setup(self, config):
-        super(MatrixFederationAgentTests, self).config_setup(config)
+        super(SygnalTwistedProxyTests, self).config_setup(config)
         config["apps"]["com.example.gcm"] = {
             "type": "tests.test_gcm.TestGcmPushkin",
             "api_key": "kii",
