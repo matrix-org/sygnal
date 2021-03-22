329f3aadd646c3c6e0d9e3e414ec209ea7c7027c
Bruno Windels <brunow@element.io>
2021-03-22 16:03:54 +0100

    more import ordering

--- sygnal/utils.py
+++ sygnal/utils.py
@@ -12,10 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import re
 from logging import LoggerAdapter
 
 from twisted.internet.defer import Deferred
-import re
 
 
 async def twisted_sleep(delay, twisted_reactor):
