b0a5a87c6ddb59c8b83c71158047353e8aa47fa6
H. Shay <shaysquared@gmail.com>
2021-04-21 12:05:34 -0700

    added missing comma

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     ],
     extras_require={
         "dev": [
-            "coverage~=5.5"
+            "coverage~=5.5",
             "black==20.8b1",
             "flake8==3.9.0",
             "isort~=5.0",
