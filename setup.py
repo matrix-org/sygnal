06d55e420d21a2d66860176b419950bd60f49720
kibablu <kibablu16@gmail.com>
2021-04-22 17:57:15 +0300

    Fix improved static type checking setup.py
    fixes #201
    
    Signed-off-by: Omar Mohamed <kibablu16@gmail.com>

--- setup.py
+++ setup.py
@@ -16,8 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-from typing import Any
+from typing import Union
+from os import PathLike
+import os.path
 
 from setuptools import find_packages, setup
 
@@ -26,7 +27,7 @@ from setuptools import find_packages, setup
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
-def read(fname: Any) -> Any:
+def read(fname: Union[str, PathLike[str]]) -> str:
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
