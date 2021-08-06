679121f9de4e93257d81f7c43dd207e363ec33bb
reivilibre <38398653+reivilibre@users.noreply.github.com>
2021-08-06 14:25:02 +0100

    Add testing for old dependencies (#242)
    Co-authored-by: Richard van der Hoff <1389908+richvdh@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -17,53 +17,61 @@
 # limitations under the License.
 
 import os.path
-from os import PathLike
-from typing import Union
+from typing import Sequence
 
 from setuptools import find_packages, setup
 
+INSTALL_REQUIRES = [
+    "Twisted>=19.7",
+    "prometheus_client>=0.7.0,<0.8",
+    "aioapns>=1.10",
+    "cryptography>=2.6.1",
+    "pyyaml>=5.1.1",
+    "service_identity>=18.1.0",
+    "jaeger-client>=4.0.0",
+    "opentracing>=2.2.0",
+    "sentry-sdk>=0.10.2",
+    "zope.interface>=4.6.0",
+    "idna>=2.8",
+    "importlib_metadata",
+    "pywebpush>=1.13.0",
+    "py-vapid>=1.7.0",
+]
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname: Union[str, "PathLike[str]"]) -> str:
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+EXTRAS_REQUIRE = {
+    "dev": [
+        "coverage~=5.5",
+        "black==21.6b0",
+        "flake8==3.9.0",
+        "isort~=5.0",
+        "mypy==0.812",
+        "mypy-zope==0.3.0",
+        "tox",
+    ]
+}
 
 
-setup(
-    name="matrix-sygnal",
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    description="Reference Push Gateway for Matrix Notifications",
-    use_scm_version=True,
-    python_requires=">=3.7",
-    setup_requires=["setuptools_scm"],
-    install_requires=[
-        "Twisted>=19.2.1",
-        "prometheus_client>=0.7.0,<0.8",
-        "aioapns>=1.10",
-        "cryptography>=2.1.4",
-        "pyyaml>=5.1.1",
-        "service_identity>=18.1.0",
-        "jaeger-client>=4.0.0",
-        "opentracing>=2.2.0",
-        "sentry-sdk>=0.10.2",
-        "zope.interface>=4.6.0",
-        "idna>=2.8",
-        "importlib_metadata",
-        "pywebpush>=1.13.0",
-        "py-vapid>=1.7.0",
-    ],
-    extras_require={
-        "dev": [
-            "coverage~=5.5",
-            "black==21.6b0",
-            "flake8==3.9.0",
-            "isort~=5.0",
-            "mypy==0.812",
-            "mypy-zope==0.3.0",
-            "tox",
-        ]
-    },
-    long_description=read("README.rst"),
-)
+def read_file(path_segments: Sequence[str]) -> str:
+    """Read a file from the package.
+
+    Params:
+        path_segments: a list of strings to join to make the path.
+    """
+    here = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(here, *path_segments)
+    with open(file_path) as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name="matrix-sygnal",
+        packages=find_packages(exclude=["tests", "tests.*"]),
+        description="Reference Push Gateway for Matrix Notifications",
+        use_scm_version=True,
+        python_requires=">=3.7",
+        setup_requires=["setuptools_scm"],
+        install_requires=INSTALL_REQUIRES,
+        extras_require=EXTRAS_REQUIRE,
+        long_description=read_file(("README.rst",)),
+    )
