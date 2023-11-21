matrix-org/sygnal

commit 0ced8bda3c759b1584d327205245162da64488a4

Use setuptools_scm for version number
Hardcoded version numbers are rubbish.

sygnal/__init__.py
@@ -1,1 +1,22 @@
-__version__ = "0.2.4"
+# -*- coding: utf-8 -*-
+# Copyright 2020 The Matrix.org Foundation C.I.C.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+   pass

VERSION
@@ -1,1 +1,0 @@
-0.2.4

setup.py
@@ -31,9 +31,10 @@ def read(fname):
 
 setup(
     name="matrix-sygnal",
-    version=read("VERSION").strip(),
     packages=find_packages(exclude=["tests", "tests.*"]),
     description="Reference Push Gateway for Matrix Notifications",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     install_requires=[
         "Twisted>=19.2.1",
         "prometheus_client>=0.7.0,<0.8",
@@ -45,6 +46,7 @@ setup(
         "sentry-sdk>=0.10.2",
         "zope.interface>=4.6.0",
         "idna>=2.8",
+        "importlib_metadata",
     ],
     long_description=read("README.rst"),
 )

commit e5bf111dba277a02ca7da0e3053f456a7ff249e5

Lookup Sygnal version using the correct name
We passed it a module name [1] instead of the "distribution name"(?)

Broken in 0ced8bda3c759b1584d327205245162da64488a4, so Sygnal 0.5.0.

[1]: https://docs.python.org/3/reference/import.html#name__

sygnal/__init__.py
@@ -16,7 +16,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version(__name__)
+    __version__ = version("matrix-sygnal")
 except PackageNotFoundError:
     # package is not installed
     pass