f308a28412292090293d4ccef20b8d680535d037
Mathieu Velten <mathieu@velten.xyz>
2024-05-15 21:54:51 +0200

    Update docker build and workflows to Python 3.11 (#373)
    * Update docker build and workflows to Python 3.11
    
    * Add changelog and fix test

--- tests/asyncio_test_helpers.py
+++ tests/asyncio_test_helpers.py
@@ -1,7 +1,7 @@
 import logging
 import types
 from asyncio import AbstractEventLoop, transports
-from asyncio.protocols import BaseProtocol, Protocol
+from asyncio.protocols import BaseProtocol, BufferedProtocol, Protocol
 from asyncio.transports import Transport
 from contextvars import Context
 from typing import Any, Callable, List, Optional, Tuple
@@ -164,8 +164,13 @@ class MockTransport(Transport):
 
     def pretend_to_receive(self, data: bytes) -> None:
         proto = self.get_protocol()
-        assert isinstance(proto, Protocol)
-        proto.data_received(data)
+        if isinstance(proto, Protocol):
+            proto.data_received(data)
+        elif isinstance(proto, BufferedProtocol):
+            data_len = len(data)
+            b = proto.get_buffer(data_len)
+            b[0:data_len] = data  # type: ignore[index]
+            proto.buffer_updated(data_len)
 
     def set_protocol(self, protocol: BaseProtocol) -> None:
         self.protocol = protocol
