1d0b1f0369a71a4ccbd2091284bd538efb448ebf
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2025-05-12 11:26:01 +0100

    Add proxy test to CI

--- scripts-dev/proxy-test/setup.sh
+++ scripts-dev/proxy-test/setup.sh
@@ -19,5 +19,6 @@ if [ ! -f mitmproxy/mitmproxy-ca.pem ]; then
   # According to instructions from https://docs.mitmproxy.org/stable/concepts/certificates/
   openssl req -x509 -new -nodes -key mitmproxy/ca.key -sha256 -out mitmproxy/ca.crt -addext keyUsage=critical,keyCertSign -subj '/CN=MyOrg Root CA/C=GB/ST=MySt/L=MyL/O=MyOrg'
   cat mitmproxy/ca.key mitmproxy/ca.crt > mitmproxy/mitmproxy-ca.pem
+  chmod ugo+rwX mitmproxy/ca.crt mitmproxy/ca.key mitmproxy/mitmproxy-ca.pem
 fi
 
